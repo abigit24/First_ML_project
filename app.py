9e0eaeb8e71d585491754a35d57761cc5e5c6bce
abigit24 <abi.hermy@gmail.com>
2022-06-18 12:06:35 +0530

    changed the display text

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Staring Machine Learning Project"
+    return "Going to start house predictions"
 
 if __name__ == "__main__":
     app.run(debug=True)
