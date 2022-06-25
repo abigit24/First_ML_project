a0629ab94a3db1733b37ef49b206102e54846765
abigit24 <abi.hermy@gmail.com>
2022-06-25 10:11:02 +0530

    Added the folder structure in the housing folder

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from distutils.log import debug
 from flask import Flask
-
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Going to start house predictions"
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
+    return "CI CD pipeline has been established."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    app.run(debug=False)
